ac18babc8287ec89176d3c00b72ef5d1a109f262
Connor Gallopo <connor.gallopo@me.com>
2018-08-17 08:37:37 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,15 @@
-// Code ControlledInput Component Here
+// Code ControlledInput Component Hereclass Form extends Component {
+state = {
+  firstName: "John",
+  lastName: "Henry"
+}
+
+render() {
+  return (
+    <form>
+      <input type="text" name="firstName" value={this.state.firstName} />
+      <input type="text" name="lastName" value={this.state.lastName} />
+    </form>
+  )
+}
+}
