112a901eb192c05762e3037bba7601e5973d2946
Gingertonic <bethmschofield@gmail.com>
2018-09-18 22:05:10 +1000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,32 @@
 // Code ControlledInput Component Here
+import React from 'react';
+
+export default class Form extends React.Component {
+  state = { firstName: "Beti", lastName: "Schofield" }
+
+  handleFirstNameChange = (event) => {
+    this.setState({firstName: event.target.value})
+  }
+
+  confirmFirstName = (event) => {
+    if (event.target.value !== "Beti") {
+      this.setState({firstName: "No! Only Beti!"})
+    } else {
+      this.handleFirstNameChange(event)
+    }
+  }
+
+  handleLastNameChange = (event) => {
+    this.setState({lastName: event.target.value})
+  }
+
+  render(){
+    return(
+      <div>
+        <input type="text" onChange={this.confirmFirstName} value={this.state.firstName} />
+        <input type="text" onChange={this.handleLastNameChange} value={this.state.lastName} />
+        <div>{this.state.firstName} {this.state.lastName}</div>
+      </div>
+    )
+  }
+}
