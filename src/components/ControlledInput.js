b4a67de69aa7d6e0966b95bdb60326d1b21fc6b9
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-06 20:39:34 -0400

    Implement ControlledInput

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,38 @@
 // Code ControlledInput Component Here
+import React from 'react'
+
+class ControlledInput extends React.Component {
+    state = {
+        value: this.props.initialValue,
+    }
+
+    handleChange = event => {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.sendFormDataSomewhere(this.state)
+    }
+
+    sendFormDataSomewhere(formData){
+        //
+    }
+
+    render() {
+        return (
+            <form onSubmit={event => this.handleSubmit(event)}>
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+            </form>
+        )
+    }
+}
+
+export default ControlledInput
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ControlledInput from './components/ControlledInput'
+
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControlledInput initialValue="BANANAS FOSTER" />,
   document.getElementById('root')
 );
