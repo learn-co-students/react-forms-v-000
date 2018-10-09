67b0ce5c364a65eb1fd18384d8bc68739a6b7f87
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-08 19:55:14 -0700

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,22 @@
 // Code ControlledInput Component Here
+class Form extends Component {
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+ 
+  render() {
+    return (
+      <form>
+        <input type="text" id="firstName" onChange={event => this.handleChange(event)} value={this.state.firstName} />
+        <input type="text" id="lastName" onChange={event => this.handleChange(event)} value={this.state.lastName} />
+      </form>
+    )
+  }
+}
