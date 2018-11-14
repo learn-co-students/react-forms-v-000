fc3a462f9f074b561311cbf89ae28d5a5fe2ba37
tsyaeger <tsyaeger@gmail.com>
2018-11-13 20:12:20 -0800

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,20 @@
 // Code ControlledInput Component Here
+import React, { Component } from 'react';
+
+class ControlledInput extends Component {
+
+
+    this.state = {
+      value: '',
+    };
+  }
+
+
+  render() {
+    return (
+      <input type="text" value={this.state.value}/>
+    );
+  }
+}
+
+export default ControlledInput;
