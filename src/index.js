0ac9937ea91567eb62e23591a98e99516e663aa0
MiriamPeskowitz <miriam5000@gmail.com>
2018-12-13 15:25:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <div>Mount Components Here</div>,
+//   document.getElementById('root')
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+ 
+import ControlledInput from './components/ControlledInput';
+ 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControlledInput />,
   document.getElementById('root')
 );
