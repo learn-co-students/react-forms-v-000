e3dbdf40fd4052f96b58d0b7c04b669e915ba5f9
Hector-Ariceaga <crablogni@hotmail.com>
2019-01-25 11:14:42 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import ControlledInput from './components/ControlledInput';
 
 ReactDOM.render(
-  <div><ControlledInput</div>,
+  <div><ControlledInput /></div>,
   document.getElementById('root')
 );
