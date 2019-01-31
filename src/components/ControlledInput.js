513a06c7ea045f55011c73c5804d40cd8c168541
btmccollum <btmcco8@gmail.com>
2019-01-31 10:32:24 -0600

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,26 @@
 // Code ControlledInput Component Here
+import React, { Component } from 'react';
+
+export default class Form extends Component {
+    state = {
+        firstName: "Dudley",
+        lastName: "Brown"
+    }
+
+    handleChange = (event) => {
+        const targetKey = event.target.name;
+        const targetValue = event.target.value;
+        const toUpdate = { [targetKey]: targetValue }
+        
+        this.setState(toUpdate);
+    }
+
+    render() {
+        return (
+            <form>
+                <input type="text" name="firstName" onChange={event => this.handleChange(event)} value={this.state.firstName} />
+                <input type="text" name="lastName" onChange={event => this.handleChange(event)} value={this.state.lastName} />
+            </form>
+        )
+    }
+}
