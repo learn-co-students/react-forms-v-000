61ccb0299e3a1154dd151b69740b221da4ccae0e
Mallory <mallory.feuer@gmail.com>
2019-04-17 21:43:55 -0400

    add handleChange function to onChange event, import and export component

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,18 +1,25 @@
 // Code ControlledInput Component Here
-import React from 'react';
+import React, { Component } from 'react';
 
 class ControlledInput extends Component {
   state = {
-    firstName: "John",
-    lastName: "Henry"
+    firstName: "",
+    lastName: ""
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
     return (
       <form>
-        <input type="text" name="firstName" value={this.state.firstName} />
-        <input type="text" name="lastName" value={this.state.lastName} />
+        <input type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName} />
+        <input type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName} />
       </form>
     )
   }
 }
+
+export default ControlledInput;
