d2dd21bb912cba4a23c7696aa850335104953ef2
dannyenfp <daniel.valmas@gmail.com>
2019-09-04 20:04:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+// you must first place the react 
+// then the reactDOM from 'react-dom' 
+// then the controlled input
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+ 
+import ControlledInput from './components/ControlledInput';
+ 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControlledInput />,
   document.getElementById('root')
 );
