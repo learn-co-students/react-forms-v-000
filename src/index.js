4097b64d89c01c7a9c8802dd08e2af147aaedafd
John Blank <veloblank@gmail.com>
2019-09-30 19:21:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "./components/Form";
+import ParentComponent from "./components/ParentComponent";
+import DisplayData from "./components/DisplayData";
 
 ReactDOM.render(
   <div>
+    <ParentComponent />
     <Form />
+    <DisplayData />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
