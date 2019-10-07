95bb7b27e8deed9bf8df266c3bcfc4198d9296d1
valeria campilongo <cvaleriac@hotmail.it>
2019-10-07 21:10:03 +0000

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,32 @@
 import React from 'react';
 
-class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
-  }
+class ControlledInput extends React.Component {
+    state = {
+        value: '',
+    }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
+    handleChange = event => {
+        this.setState({
+        value: event.target.value,
+        });
+    }
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
+    handleSubmit = event => {
+        event.preventDefault()
+        this.sendFormDataSomewhere(this.state)
+    }
 
-  render() {
-    return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
-    )
-  }
+    render() {
+        return (
+            <form onSubmit={event => this.handleSubmit(event)}>
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+            </form>
+        );
+    }
 }
 
-export default Form;
+export default ControlledInput;
