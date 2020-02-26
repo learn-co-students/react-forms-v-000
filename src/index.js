5e75df89217c3fa161cbf8ca4d4129482b05a3ce
yuanxizhang <yuanxi.zhang@gmail.com>
2020-02-26 11:20:53 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
 
+import React, { Component, PropTypes } from 'react';
+
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent />
   </div>,
   document.getElementById('root')
 );
