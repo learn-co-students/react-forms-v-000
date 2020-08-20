855a269fe022a93db0ca1dc6febcd1f5889863ed
Kat Tochowicz <ktochow1@binghamton.edu>
2020-08-20 13:35:57 +0200

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,24 +6,15 @@ class Form extends React.Component {
     lastName: "Henry"
   }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
-
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
 
   render() {
     return (
+      <div>
       <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <input type="text" name="firstName" onChange={event => this.props.handleChange(event)} value={this.props.formData.firstName} />
+        <input type="text" name="lastName" onChange={event => this.handleChange(event)} value={this.props.formData.lastName} />
       </form>
+      </div>
     )
   }
 }
