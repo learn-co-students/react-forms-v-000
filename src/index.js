d0ae42ac308b0617bb7557533828ca55eb2caf4f
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-11 20:30:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <div>
-    <Form />
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
