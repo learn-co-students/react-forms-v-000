22580fa415a1ecdf25a4dabe75bc1b2716290458
Laura Travin <laura.travin@gmail.com>
2021-06-01 17:24:25 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,15 @@ class Form extends React.Component {
     return (
       <div>
         <form>
+        <label>First Name</label>
           <input
             type="text"
             name="firstName"
             onChange={event => this.props.handleChange(event)}
             value={this.props.formData.firstName}
           />
+          <br/>
+          <label>Last Name</label>
           <input
             type="text"
             name="lastName"
